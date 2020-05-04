eb939c34a4bc8324806615f2758504b84ac99227
Kimberley Spicer <kspicerny@gmail.com>
2020-05-04 18:59:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Hello, Hello, Neil deGrasse Tyson"
+var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = topic + specialGuest + "!"';
 
-var topic = "Let\'s talk about Let\'s talk about space...";
+var topic = "Let\'s talk about space.";
 
 var conversation = `${topic}`;
 
